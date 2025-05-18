3f7c3b38bf9fbf49b9a9a8d99cf5d2f90960b825
ansidium <10288054+ansidium@users.noreply.github.com>
2025-05-18 23:33:04 +0300

    Revert "Add Delphi decoder skeleton and repo guidelines"

--- delphi/LZMA2Dec.pas
+++ delphi/LZMA2Dec.pas
@@ -5,19 +5,6 @@ interface
 uses
   SysUtils;
 
-const
-  kLzma2LcLpMax = 4;
-
-  { Error value returned by helper functions when the dictionary
-    property is invalid. }
-  LZMA2_DICT_ERROR = High(PtrUInt);
-
-  { Decoder finish modes }
-  LZMA_FINISH_ANY = 0;
-  LZMA_FINISH_END = 1;
-
-type
-  TLZMA2_props = record
 type
   LZMA2_prob = Cardinal;
   PLZMA2_prob = ^LZMA2_prob;
@@ -83,14 +70,6 @@ type
     dic_size: Cardinal;
   end;
 
-  PLZMA2_prob = ^Cardinal;
-
-  TLZMA2_DCtx = record
-    prop: TLZMA2_props;
-    dic: PByte;
-    dic_pos: SizeUInt;
-    dic_buf_size: SizeUInt;
-
   LZMA2_state = (
     LZMA2_STATE_CONTROL,
     LZMA2_STATE_DATA,
@@ -103,7 +82,6 @@ type
     dic: PByte;
     dic_pos: NativeUInt;
     dic_buf_size: NativeUInt;
-
     buf: PByte;
     probs_1664: PLZMA2_prob;
     range: Cardinal;
@@ -112,12 +90,10 @@ type
     check_dic_size: Cardinal;
     reps: array[0..3] of Cardinal;
     state: Cardinal;
-
     state2: LZMA2_state;
     remain_len: Cardinal;
     pack_size: NativeUInt;
     unpack_size: NativeUInt;
-
     control: Byte;
     need_init_dic: Byte;
     need_init_state: Byte;
@@ -125,57 +101,6 @@ type
     need_init_prop: Byte;
     need_flush: Byte;
     ext_dic: Byte;
-
-    pad2_: Byte;
-    probs: Pointer; { placeholder for probability table }
-  end;
-
-procedure LZMA_constructDCtx(var ctx: TLZMA2_DCtx);
-procedure LZMA_destructDCtx(var ctx: TLZMA2_DCtx);
-function LZMA2_getDictSizeFromProp(dict_prop: Byte): SizeUInt;
-function LZMA2_initDecoder(var ctx: TLZMA2_DCtx; dict_prop: Byte;
-  dic: PByte; dic_buf_size: SizeUInt): SizeUInt;
-
-implementation
-
-procedure LZMA_constructDCtx(var ctx: TLZMA2_DCtx);
-begin
-  ctx.dic := nil;
-  ctx.ext_dic := 1;
-  ctx.state2 := LZMA_FINISH_END;
-  ctx.probs_1664 := nil;
-  ctx.probs := nil;
-end;
-
-procedure LZMA_destructDCtx(var ctx: TLZMA2_DCtx);
-begin
-  ctx.dic := nil;
-  ctx.probs := nil;
-end;
-
-function LZMA2_getDictSizeFromProp(dict_prop: Byte): SizeUInt;
-begin
-  if dict_prop > 40 then
-    Exit(LZMA2_DICT_ERROR);
-  if dict_prop = 40 then
-    Result := High(SizeUInt)
-  else
-    Result := (SizeUInt(2 or (dict_prop and 1)) shl ((dict_prop div 2) + 11));
-end;
-
-function LZMA2_initDecoder(var ctx: TLZMA2_DCtx; dict_prop: Byte;
-  dic: PByte; dic_buf_size: SizeUInt): SizeUInt;
-begin
-  Result := LZMA2_getDictSizeFromProp(dict_prop);
-  ctx.dic := dic;
-  ctx.dic_buf_size := dic_buf_size;
-  ctx.need_init_dic := 1;
-  ctx.need_init_state2 := 1;
-  ctx.need_init_prop := 1;
-end;
-
-end.
-
     pad__: Byte;
     probs: array[0..PROBS_ARRAY_SIZE - 1] of LZMA2_prob;
   end;
--- delphi/README.md
+++ delphi/README.md
@@ -1,13 +1,5 @@
 # Delphi Port (Experimental)
 
-This directory contains an experimental translation of portions of Fast LZMA2
-into the Delphi language.  Currently the range encoder has been partially
-rewritten and an initial decoder context skeleton is provided.
-The code serves as a reference for how the original C implementation can be
-mapped to Pascal types and routines.  It is **not** a complete reimplementation
-of the library. Further work would be required to port all compression and
-decompression features.
-
 This directory contains an experimental partial translation of Fast LZMA2's
 lzma2-delphi-j9h89v
 range encoder and decoder to the Delphi language.  It demonstrates how the
@@ -15,7 +7,7 @@ original C implementation can be mapped to Pascal types and functions.  The
 port is **not** yet a complete reimplementation of the entire library; only a
 subset of the range coder and decoder logic has been rewritten.  Further work
 is required to port all compression and decompression features.
-
+=======
 range encoder to the Delphi language.  It demonstrates how the original C
 implementation can be mapped to Pascal types and functions.  The port is not a
 complete reimplementation of the entire library; only a subset of the range
